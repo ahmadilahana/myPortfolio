fb0c2be37d50dd024f2fc7ada69e231d9dd738b2
ahmadilahana <ahmadilahana@gmailcom>
2021-12-04 01:03:14 +0700

    navbar client

---
+++ frontend/src/components/MainNav.vue
@@ -0,0 +1,15 @@
+<template>
+    <client-nav></client-nav>
+</template>
+
+<script>
+import ClientNav from './client/nav.vue'
+export default {
+    components: {
+        ClientNav
+    },
+    mounted(){
+        console.log(this.$route.path)
+    }
+}
+</script>
---
+++ frontend/src/components/client/nav.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-nav>
+        <b-nav-item>
+            <h3>
+                MY<small class="text-muted">PORTFOLIO</small>
+            </h3>
+        </b-nav-item>
+        <b-nav-item class="align-self-center nav-menu">Active</b-nav-item>
+        <b-nav-item class="align-self-center nav-menu">Link</b-nav-item>
+        <b-nav-item class="align-self-center nav-menu">Another Link</b-nav-item>
+        <b-nav-item class="align-self-center nav-menu">Disabled</b-nav-item>
+    </b-nav>
+</template>
+
+<style>
+.nav-menu:hover{
+    background: rgb(247, 246, 252);
+    border-radius: .25rem !important;
+}
+.nav-menu a:hover{
+    font-weight: bold;
+}
+</style>
