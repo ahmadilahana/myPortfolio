b2448f8b84e0f973c8bcffef3c3d46e9a34b358f
ahmadilahana <ahmadilahana@gmailcom>
2021-12-07 04:21:55 +0700

    update project and add contact

--- frontend/src/components/client/nav.vue
+++ frontend/src/components/client/nav.vue
@@ -1,19 +1,22 @@
 <template>
-    <b-nav class="pr-5 fixed-top bg-white">
-        <b-nav-item >
-            <h3 class="font-weight-bold">
+    <b-navbar toggleable='lg' type="light" variant="white" class="fixed-top">
+        <b-navbar-brand >
+            <h3 class="font-weight-bold text-info">
                 MY<small class="text-muted font-weight-bold">PORTFOLIO</small>
             </h3>
-        </b-nav-item>
-        <!-- <div class="ml-auto"> -->
-            <b-nav-item class="align-self-center nav-menu ml-auto" href="#home">Home</b-nav-item>
-            <b-nav-item class="align-self-center nav-menu" href="#about">About</b-nav-item>
-            <b-nav-item class="align-self-center nav-menu" href="#experience">Experience</b-nav-item>
-            <b-nav-item class="align-self-center nav-menu" href="#skill">Skill</b-nav-item>
-            <b-nav-item class="align-self-center nav-menu">Project</b-nav-item>
-            <b-nav-item class="align-self-center nav-menu">Contact</b-nav-item>
-        <!-- </div> -->
-    </b-nav>
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="#home">Home</b-nav-item>
+                <b-nav-item href="#about">About</b-nav-item>
+                <b-nav-item href="#experience">Experience</b-nav-item>
+                <b-nav-item href="#skill">Skill</b-nav-item>
+                <b-nav-item href="#project">Project</b-nav-item>
+                <b-nav-item href="#contact">Contact</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
 </template>
 
 <style>
--- frontend/src/views/client/home.vue
+++ frontend/src/views/client/home.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <div class="container-fluid text-center pl-0 pr-0" style="background-color: #1dcaff" id="home">
+    <div class="container-fluid text-center pl-0 pr-0 pb-0" style="background-color: #1dcaff" id="home">
         <img src="../../asset/img/img-1.jpg" class="rounded-circle" style="border: 4px solid white" height="150px" width="150px" alt="">
         <h1 class="display-5 text-light font-weight-bold">Ahmat Arif Ilahana</h1>
         <div class="d-flex justify-content-center">
@@ -10,7 +10,7 @@
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,288L0,64L36.9,64L36.9,160L73.8,160L73.8,256L110.8,256L110.8,288L147.7,288L147.7,192L184.6,192L184.6,192L221.5,192L221.5,96L258.5,96L258.5,64L295.4,64L295.4,192L332.3,192L332.3,96L369.2,96L369.2,256L406.2,256L406.2,64L443.1,64L443.1,256L480,256L480,96L516.9,96L516.9,192L553.8,192L553.8,160L590.8,160L590.8,224L627.7,224L627.7,32L664.6,32L664.6,64L701.5,64L701.5,288L738.5,288L738.5,64L775.4,64L775.4,192L812.3,192L812.3,192L849.2,192L849.2,128L886.2,128L886.2,256L923.1,256L923.1,96L960,96L960,96L996.9,96L996.9,224L1033.8,224L1033.8,96L1070.8,96L1070.8,64L1107.7,64L1107.7,128L1144.6,128L1144.6,96L1181.5,96L1181.5,288L1218.5,288L1218.5,32L1255.4,32L1255.4,64L1292.3,64L1292.3,160L1329.2,160L1329.2,32L1366.2,32L1366.2,32L1403.1,32L1403.1,192L1440,192L1440,320L1403.1,320L1403.1,320L1366.2,320L1366.2,320L1329.2,320L1329.2,320L1292.3,320L1292.3,320L1255.4,320L1255.4,320L1218.5,320L1218.5,320L1181.5,320L1181.5,320L1144.6,320L1144.6,320L1107.7,320L1107.7,320L1070.8,320L1070.8,320L1033.8,320L1033.8,320L996.9,320L996.9,320L960,320L960,320L923.1,320L923.1,320L886.2,320L886.2,320L849.2,320L849.2,320L812.3,320L812.3,320L775.4,320L775.4,320L738.5,320L738.5,320L701.5,320L701.5,320L664.6,320L664.6,320L627.7,320L627.7,320L590.8,320L590.8,320L553.8,320L553.8,320L516.9,320L516.9,320L480,320L480,320L443.1,320L443.1,320L406.2,320L406.2,320L369.2,320L369.2,320L332.3,320L332.3,320L295.4,320L295.4,320L258.5,320L258.5,320L221.5,320L221.5,320L184.6,320L184.6,320L147.7,320L147.7,320L110.8,320L110.8,320L73.8,320L73.8,320L36.9,320L36.9,320L0,320L0,320Z"></path></svg>
     </div>
-    <div class="container-fluid pl-0 pr-0 mb-5" id="about">
+    <div class="container-fluid pl-0 pr-0" id="about">
         <div class="row mx-0 px-0">
             <div class="col text-center">
                 <h3 class="font-weight-bold">About Me</h3>
@@ -28,15 +28,15 @@ bekerja secara tim atau individu.</p>
         </div>
     </div>
     
-    <div class="container-fluid pl-0 pr-0 pb-5" id="experience" style="border: 10px solid #1dcaff">
+    <div class="container-fluid pl-0 pr-0" id="experience" style="border: 10px solid #1dcaff">
         <div class="row mx-0 px-0">
             <div class="col text-center">
                 <h3 class="font-weight-bold">Experience</h3>
             </div>
         </div>
-        <div class="d-flex justify-content-md-center">
-            <div class="m-5 border p-3 w-50">
-                <b-tabs content-class="mt-3" align='center'>
+        <div class="row justify-content-md-center">
+            <div class="border p-3 col-md-6">
+                <b-tabs responsive content-class="mt-3" align='center'>
                     <b-tab title="Educational Experience" active>
                         <b-table :items="education" thead-class="hidden_header"/>
                     </b-tab>
@@ -48,7 +48,7 @@ bekerja secara tim atau individu.</p>
         </div>
     </div>
 
-    <div class="container-fluid pl-0 pr-0 pb-5 text-white" id="skill" style="background-color: #1dcaff">
+    <div class="container-fluid pl-0 pr-0 text-white" id="skill" style="background-color: #1dcaff">
         <div class="row mx-0 px-0">
             <div class="col text-center">
                 <h3 class="font-weight-bold">Skill</h3>
@@ -57,16 +57,16 @@ bekerja secara tim atau individu.</p>
         <div class="row justify-content-center mt-3 mx-0 px-0">
             <div class="col-md-4">
                 <div v-for="skill in skills" :key="skill" class="row mb-1">
-                    <div class="col-sm-3 font-weight-bold">{{ skill.variant }}</div>
-                    <div class="col-sm-9 pt-1">
+                    <div class="col-sm-4 font-weight-bold">{{ skill.variant }}</div>
+                    <div class="col-sm-8 pt-1">
                         <b-progress :value="skill.value" :max="max" :key="skill.variant" show-value></b-progress>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div v-for="skill in skills2" :key="skill" class="row mb-1">
-                    <div class="col-sm-3 font-weight-bold">{{ skill.variant }}</div>
-                    <div class="col-sm-9 pt-1">
+                    <div class="col-sm-4 font-weight-bold">{{ skill.variant }}</div>
+                    <div class="col-sm-8 pt-1">
                         <b-progress :value="skill.value" :max="max" :key="skill.variant" show-value></b-progress>
                     </div>
                 </div>
@@ -74,13 +74,86 @@ bekerja secara tim atau individu.</p>
         </div>
     </div>
 
-    <div class="container-fluid pl-0 pr-0 pb-5" id="project" style="border: 10px solid #1dcaff">
+    <div class="container-fluid pl-0 pr-0" id="project" style="border: 10px solid #1dcaff">
         <div class="row mx-0 px-0">
             <div class="col text-center">
                 <h3 class="font-weight-bold">Project</h3>
             </div>
         </div>
-        
+        <div class="row justify-content-md-center mt-3 px-5">
+            <div class="col-md-4 mt-4 d-flex justify-content-md-center">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="../../asset/img/bank-sampah.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Bank Sampah Ku</h5>
+                        <p class="card-text">Sebuah Admin Web Sederhana untuk sistem bank sampah.
+                            <br>Email: admin@email.com<br>Password: 123123
+                        </p>
+                        <a href="https://bank-sampah-ku.herokuapp.com" target="_blank" class="btn btn-primary">Visit Web</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mt-4 d-flex justify-content-md-center">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="../../asset/img/nekokepo.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">NekoKepo</h5>
+                        <p class="card-text">Sebuah web company profile dari restoran NekoKepo.
+                        </p>
+                        <a href="https://nekokepo.herokuapp.com" target="_blank" class="btn btn-primary">Visit Web</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mt-4 d-flex justify-content-md-center">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="../../asset/img/myportfolio.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">MYPorfolio</h5>
+                        <p class="card-text">Sebuah Web Porfolio pribadi saya sendiri.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mt-4 d-flex justify-content-md-center">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="../../asset/img/restapi.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Adolloka</h5>
+                        <p class="card-text">Sebuah RestApi untuk web e-commerce sederhana dengan Laravel.</p>
+                        <a href="https://documenter.getpostman.com/view/15056203/TzJx7va8" target="_blank" class="btn btn-primary">Visit API Documentation</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid pl-0 pr-0 text-white" id="contact" style="background-color: #1dcaff">
+        <div class="row mx-0 px-0">
+            <div class="col text-center">
+                <h3 class="font-weight-bold">Contact</h3>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-3 mx-0 px-0">
+            <div class="col-md-4">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea class="form-control" id="description" rows="3"></textarea>
+                    </div>
+                    <div class="d-flex">
+                        <button class="btn btn-md w-25 btn-light text-primary ml-auto">Send</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -133,6 +206,7 @@ export default {
 
 .container-fluid{
     padding-top: 6rem;
+    padding-bottom: 6rem;
 }
 .hidden_header {
   display: none;
