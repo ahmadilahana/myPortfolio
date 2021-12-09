0a19585f0e3fc72c58a69d3354eef980d498d05d
ahmadilahana <ahmadilahana@gmailcom>
2021-12-09 19:18:04 +0700

    update home server

--- frontend/src/views/server/home.vue
+++ frontend/src/views/server/home.vue
@@ -1,24 +1,38 @@
 <template>
-    <div class="container bg-light border" style="min-height: 650px">
+    <div class="container bg-primary border" style="min-height: 650px">
         <div class="row" style="min-height: 650px">
-            <div class="col-4 d-flex flex-column border-right px-0" id="member">
-                <div class="p-2 pl-4 border-bottom">
+            <div class="col-4 d-flex flex-column border-right py-3" id="member">
+                <div class="p-2 pl-4 mb-2 border-bottom rounded">
+                    <p class="lead m-0">nama</p>
+                    <p class="m-0">email</p>
+                </div>
+                <div class="p-2 pl-4 mb-2 border-bottom rounded">
                     <p class="lead m-0">nama</p>
                     <p class="m-0">email</p>
                 </div>
             </div>
             <div class="col-8 d-flex align-items-center">
-                <div class="d-flex w-100 justify-content-center">
+                <div class="d-flex h-100 w-100 text-white justify-content-center">
                     <h1>Welcome MYPorfolio</h1>
                 </div>
             </div>
         </div>
     </div>
 </template>
-<style>
-@media only screen and (max-width: 100px) {
-    #member {
-        display: none;
+<script>
+export default {
+    methods: {
+
     }
+}
+</script>
+<style>
+#member div{
+    background-color: white;
+}
+#member div:hover{
+    background-color: lightsteelblue;
+    color: white;
+    cursor: pointer;
 }
 </style>
