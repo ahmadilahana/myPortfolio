6cf95aebdb47abda9491b3cad7ec0e47e51adfcf
ahmadilahana <ahmadilahana@gmailcom>
2021-12-12 04:07:52 +0700

    add icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueMeta from 'vue-meta'
-import { BootstrapVue } from 'bootstrap-vue'
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
 
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(VueMeta)
 
 Vue.config.productionTip = false
--- frontend/src/views/server/home.vue
+++ frontend/src/views/server/home.vue
@@ -1,18 +1,26 @@
 <template>
-    <div class="container bg-primary border" style="min-height: 650px">
+    <div class="container-fluid bg-secondary border py-0 px-4" style="min-height: 650px">
         <div class="row" style="min-height: 650px">
-            <div class="col-4 d-flex flex-column border-right py-3" id="member">
-                <div class="p-2 pl-4 mb-2 border-bottom rounded">
+            <div class="col-3 d-flex flex-column border-right py-3" id="member">
+                <div class="p-2 pl-3 mb-2 border-bottom text-white rounded" @click="openPesan">
                     <p class="lead m-0">nama</p>
                     <p class="m-0">email</p>
                 </div>
-                <div class="p-2 pl-4 mb-2 border-bottom rounded">
+                <div class="p-2 pl-3 mb-2 border-bottom text-white rounded">
                     <p class="lead m-0">nama</p>
                     <p class="m-0">email</p>
                 </div>
             </div>
-            <div class="col-8 d-flex align-items-center">
-                <div class="d-flex h-100 w-100 text-white justify-content-center">
+            <div class="col-9 p-3" id="divPesan">
+                <nav class="nav border-bottom text-white" id="nav-menu">
+                    <div class="p-2 mb-2 border rounded" id="menu">
+                        <font-awesome-icon icon="bars" />
+                    </div>
+                </nav>
+                <div v-if="pesan" class="h-100 w-100 p-3 text-dark bg-white rounded">
+                    <h1>Welcome MYPorfolio</h1>
+                </div>
+                <div v-else class="d-flex h-100 w-100 text-white align-items-center justify-content-center">
                     <h1>Welcome MYPorfolio</h1>
                 </div>
             </div>
@@ -20,19 +28,42 @@
     </div>
 </template>
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBrandSecret } from '@fortawesome/free-brands-svg-icons'
+library.add(faBars)
 export default {
+    data(){
+        return{
+            pesan: false,
+        }
+    },
     methods: {
-
+        openPesan (){
+            this.pesan = true
+        }
     }
 }
 </script>
 <style>
-#member div{
-    background-color: white;
-}
-#member div:hover{
-    background-color: lightsteelblue;
-    color: white;
+#menu:hover, #member div:hover{
+    background-color: #626b72 !important;
+    color: white !important;
     cursor: pointer;
+}
+#nav-menu{
+    display: none !important;
+}
+@media only screen and (max-width: 576px) {
+    #member{
+        display: none !important;
+    }
+    #nav-menu{
+        display: flex !important;
+    }
+    #divPesan{
+        flex: 0 0 100%;
+        max-width: 100%;
+    }
 }
 </style>
